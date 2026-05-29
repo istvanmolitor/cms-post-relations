b43596cdbff9151301450d734a2f4223ad756357
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-05-29 17:00:06 +0200

    test

--- src/Providers/CmsPostRelationsServiceProvider.php
+++ src/Providers/CmsPostRelationsServiceProvider.php
@@ -6,9 +6,19 @@ namespace Molitor\CmsPostRelations\Providers;
 
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
+use Molitor\CmsPostRelations\Repositories\CmsPostRelationRepository;
+use Molitor\CmsPostRelations\Repositories\CmsPostRelationRepositoryInterface;
 
 class CmsPostRelationsServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->app->bind(
+            CmsPostRelationRepositoryInterface::class,
+            CmsPostRelationRepository::class,
+        );
+    }
+
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
